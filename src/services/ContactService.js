7a92254564590e39b882ed3010af44f66f57811e
benjamin.krammel <bk@internetx.com>
2020-08-27 16:11:49 +0200

    fix list

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -1,46 +1,49 @@
 let DomainRobotService = require("./DomainRobotService");
 
 class Contact extends DomainRobotService {
-  constructor(domainRobotConfig) {
-    super(domainRobotConfig);
-  }
+    constructor(domainRobotConfig) {
+        super(domainRobotConfig);
+    }
 
-  async create(model) {
-    return await this.sendPostRequest(
-        this.domainRobotConfig.url + "/contact",
-        model
-    );
-  }
+    async create(model) {
+        return await this.sendPostRequest(
+            this.domainRobotConfig.url + "/contact",
+            model
+        );
+    }
 
-  async update(model) {
-    return await this.sendPutRequest(
-        this.domainRobotConfig.url + "/contact/" + model.id,
-        model
-    );
-  }
+    async update(model) {
+        return await this.sendPutRequest(
+            this.domainRobotConfig.url + "/contact/" + model.id,
+            model
+        );
+    }
 
-  async delete(id) {
-    return await this.sendDeleteRequest(
-        this.domainRobotConfig.url + "/contact/" + id
-    );
-  }
+    async delete(id) {
+        return await this.sendDeleteRequest(
+            this.domainRobotConfig.url + "/contact/" + id
+        );
+    }
 
-  async info(id) {
-    return await this.sendGetRequest(
-        this.domainRobotConfig.url + "/contact/" + id
-    );
-  }
+    async info(id) {
+        return await this.sendGetRequest(
+            this.domainRobotConfig.url + "/contact/" + id
+        );
+    }
 
-  async list(model, keys = []) {
-    let keysString = keys.join('&keys=');
+    async list(model, keys = []) {
+        let keysString = '';
+        if (keys.length > 0) {
+            keysString = "?keys[]=" + keys.join('&keys[]=');
+        }
 
-    return await this.sendPostRequest(
-         this.domainRobotConfig.url + "/contact/_search?keys=" + keysString,
-         model
-    );
-  }
+        return await this.sendPostRequest(
+            this.domainRobotConfig.url + "/contact/_search?keys=" + keysString,
+            model
+        );
+    }
 
-  
+
 
 }
 
