f9a927ce9e8e20594407fd59cb751e8d40161374
benjamin.krammel <bk@internetx.com>
2020-09-02 09:35:19 +0200

    add logging

---
+++ src/lib/DomainRobotConfig.js
@@ -0,0 +1,25 @@
+class DomainRobotConfig {
+    constructor(domainRobotConfig) {
+        // set default values if nothing has been specified
+        this.url = domainRobotConfig.url || config.API_URL;
+        this.auth = domainRobotConfig.auth || undefined;
+
+        if (this.auth !== undefined && !this.auth.context) {
+            this.auth.context = config.API_CONTEXT;
+        }
+
+        this.session_id = domainRobotConfig.session_id || undefined;
+        this.logRequestCallback = domainRobotConfig.logRequestCallback || undefined;
+        this.logResponseCallback = domainRobotConfig.logResponseCallback || undefined;
+    }
+
+    hasLogRequestCallback(){
+        return this.logRequestCallback !== undefined;
+    }
+
+    hasLogResponseCallback() {
+        return this.logResponseCallback !== undefined;
+    }
+}
+
+module.exports = DomainRobotConfig;
