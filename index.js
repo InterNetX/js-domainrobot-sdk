859875f1416e2aa3c66d1d075c25dafdb0e5cc32
benjamin.krammel <bk@internetx.com>
2021-03-10 11:20:29 +0100

    update swagger definition files

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const DomainRobot = require("./src/Domainrobot");
 const DomainRobotHeaders = require("./src/lib/Headers");
 
+const domainrobot = require("./src/swagger/domainrobot.json");
+const pcdomains = require("./src/swagger/pcdomains.json");
+
+
 const specs = require("./src/lib/specs.json");
 const ApiFactory = require("./src/lib/Factory");
-const Backend = new ApiFactory(specs);
-const DomainRobotModels = Backend.models;
+const PcDomains = new ApiFactory(pcdomains);
+const Backend = new ApiFactory(domainrobot);
+const DomainRobotModels = Object.assign(Backend.models, PcDomains.models);
+
 const DomainRobotService = require("./src/services/DomainRobotService")
 
 //export default Domainrobot;
---
+++ src/swagger/README.md
@@ -0,0 +1,22 @@
+# Update Swagger specs
+
+```bash
+yarn
+
+#after installation is complete
+yarn update:specs
+```
+
+or with npm
+
+```bash
+npm run install
+
+#after installation is complete
+npm run update:specs
+```
+
+Run the tests to check that everything is still ok and nothing is broken.
+See tests/Readme.md for more information on how to run the tests.
+
+If the tests are all ok commit the new swagger files and publish a new npm package version.
