69d9eb070989d8ae81e38aa291b936a49b678844
Benjamin Krammel <bk@internetx.com>
2021-09-08 14:46:36 +0200

    remove console.log

--- src/services/DomainRobotService.js
+++ src/services/DomainRobotService.js
@@ -106,7 +106,6 @@ class DomainRobotService {
 
             return domainRobotResult;
         } catch (error) {
-            console.log(error)
             throw new DomainRobotException(
                 error.response.data,
                 error.response.status
