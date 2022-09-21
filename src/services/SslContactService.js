0caee65eafa29d3f07fb2810d507834887ed3fd8
Benjamin Krammel <bk@internetx.com>
2022-09-21 16:48:56 +0200

    add mock response for sslcontact->list

--- src/services/SslContactService.js
+++ src/services/SslContactService.js
@@ -1,47 +1,49 @@
 let DomainRobotService = require("./DomainRobotService");
 
 class SslContact extends DomainRobotService {
-    constructor(domainRobotConfig) {
-        super(domainRobotConfig);
+  constructor(domainRobotConfig) {
+    super(domainRobotConfig);
+  }
+
+  async create(model) {
+    return await this.sendPostRequest(
+      this.domainRobotConfig.url + "/sslcontact",
+      model
+    );
+  }
+
+  async update(model) {
+    return await this.sendPutRequest(
+      this.domainRobotConfig.url + "/sslcontact/" + model.id,
+      model
+    );
+  }
+
+  async delete(id) {
+    return await this.sendDeleteRequest(
+      this.domainRobotConfig.url + "/sslcontact/" + id
+    );
+  }
+
+  async info(id) {
+    return await this.sendGetRequest(
+      this.domainRobotConfig.url + "/sslcontact/" + id
+    );
+  }
+
+  async list(model, keys = []) {
+    this.virtualPath = ['sslcontact', 'list']
+
+    let keysString = '';
+    if (keys.length > 0) {
+      keysString = "?keys[]=" + keys.join('&keys[]=');
     }
 
-    async create(model) {
-        return await this.sendPostRequest(
-            this.domainRobotConfig.url + "/sslcontact",
-            model
-        );
-    }
-
-    async update(model) {
-        return await this.sendPutRequest(
-            this.domainRobotConfig.url + "/sslcontact/" + model.id,
-            model
-        );
-    }
-
-    async delete(id) {
-        return await this.sendDeleteRequest(
-            this.domainRobotConfig.url + "/sslcontact/" + id
-        );
-    }
-
-    async info(id) {
-        return await this.sendGetRequest(
-            this.domainRobotConfig.url + "/sslcontact/" + id
-        );
-    }
-
-    async list(model, keys = []) {
-        let keysString = '';
-        if (keys.length > 0) {
-            keysString = "?keys[]=" + keys.join('&keys[]=');
-        }
-
-        return await this.sendPostRequest(
-            this.domainRobotConfig.url + "/sslcontact/_search" + keysString,
-            model
-        );
-    }
+    return await this.sendPostRequest(
+      this.domainRobotConfig.url + "/sslcontact/_search" + keysString,
+      model
+    );
+  }
 
 
 
