d0875bc693eaccfeca093602457a0b2bf9af4a4a
artur <email@example.com>
2024-03-26 14:05:15 +0100

    user-info

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -27,7 +27,7 @@ class User extends DomainRobotService {
      * @param queries string[] {Available values: acl, profiles, customer, subscription, nameServerGroup}
      * @returns Promise<DomainRobotResult<JsonResponseDataBasicUser, number>>
      */
-    async info(user, context, queries = []) {
+    async info(user, context, queries = ['subscription']) {
 
         let queryString = "";
         if (queries.length > 0) {
